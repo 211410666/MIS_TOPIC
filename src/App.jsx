317adfe149ace34d917f46a08690fbb1b2950dc2
211410666 <211410666@o365.tku.edu.tw>
2024-06-16 14:49:19 +0800

    213

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,83 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Topic from './topic'
-import All_Topic from './All_Topic'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from 'react-hot-toast';
+
+import GlobalStyles from './styles/GlobalStyles';
+import './App_66.css';
+import {
+  HomePage,
+  Selection,
+  All_topic,
+  Exam,
+  ReviewWrong,
+  Rank
+} from './pages/index'
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60, // 1 minutes
+    },
+  },
+});
 
 
-const App = () => {
-  return(
-    <BrowserRouter>
-      <Routes>
-        <Route exact path='/' element={<Topic />} />
-        <Route path='/All_topic' element={<All_Topic />} />
-      </Routes>
-    </BrowserRouter>
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/selection',
+    element: <Selection />,
+  },
+  {
+    path: '/all_topics',
+    element: <All_topic />,
+  },
+  {
+    path: '/exam',
+    element: <Exam />,
+  },
+  {
+    path: '/review',
+    element: <ReviewWrong />,
+  },
+  {
+    path: '/rank',
+    element: <Rank />,
+  },
+]);
+
+function App_66() {
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyles />
+        <RouterProvider router={router} />
+        <Toaster
+          position='top-center'
+          gutter={12}
+          containerStyle={{ margin: '8px' }}
+          toastOptions={{
+            success: {
+              duration: 3000,
+            },
+            error: {
+              duration: 4000,
+            },
+            styles: {
+              fontSize: '16px',
+              maxWidth: '500px',
+              padding: '16px 24px',
+              backgroundColor: 'var(--color-grey-0)',
+              color: 'var(--color-grey-70)',
+            },
+          }}
+        />
+      </QueryClientProvider>
+    </>
   );
-  
 }
 
-export default App
+export default App_66;
